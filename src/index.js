c98297a3f7df1b371713bfd3a031c4fe8b875648
Felice Forby <feliceforby@gmail.com>
2019-03-24 23:53:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App'
 import manageUsers from './reducers/manageUsers'
 
 // add imports and code
-
+const store = createStore(manageUsers);
 
 ReactDOM.render(
   <Provider store={store}>
