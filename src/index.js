f3d65ea2dbee60ed43164cf7389023c0a1e26d55
Felice Forby <feliceforby@gmail.com>
2019-03-24 23:54:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import App from './App'
 import manageUsers from './reducers/manageUsers'
 
 // add imports and code
-const store = createStore(manageUsers);
+const store = createStore(
+  manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
